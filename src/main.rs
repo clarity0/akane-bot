e23dc5268bdb0bf85d5051408823cbb159ccc865
Marcos Mejias <marcosmejias@mailfence.com>
2021-04-16 23:44:46 +0800

    moved shard iterator loop to shard manager module

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,41 @@
-use std::{
-    env::var,
-    sync::Arc,
-    time::Duration,
-};
-use dotenv::dotenv;
-use serenity::{
-    prelude::*,
-    framework::{
-        StandardFramework,
-    },
-};
-use tokio::time::sleep;
-
 #[macro_use] extern crate diesel;
 mod schema;
 mod models;
 mod database;
 mod commands;
+mod handler;
+mod shard_manager;
+
+use std::{env::var, sync::Arc};
+use dotenv::dotenv;
+use serenity::{prelude::*, framework::StandardFramework};
 use commands::general::*;
 use commands::moderator::*;
-mod handler;
 use handler::Handler;
-mod shard_manager;
-use shard_manager::ShardManagerContainer;
+use shard_manager::{ShardManagerContainer,shard_iterator_task};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     let token = var("AKANE_BOT_TOKEN").expect("Bot token not found");
+
     let framework = StandardFramework::new()
         .configure(|c | c
             .prefix("!")
             .delimiter(' '))
         .group(&GENERAL_GROUP)
         .group(&MODERATOR_GROUP);
-        
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
 
-    let manager = client.shard_manager.clone();
-
     {
         let mut data = client.data.write().await;
         data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
     }
-
-    tokio::spawn(async move {
-        loop {
-            sleep(Duration::from_secs(60)).await;
-
-            let lock = manager.lock().await;
-            let shard_runners = lock.runners.lock().await;
-
-            for (id, runner) in shard_runners.iter() {
-                println!(
-                    "Shard ID {} is {} with a latency of {:?}",
-                    id,
-                    runner.stage,
-                    runner.latency,
-                );
-            }
-        }
-    });
+    shard_iterator_task(client.shard_manager.clone());
     
     if let Err(msg) = client.start_shards(1).await {
         println!("Error: {:?}", msg);
--- src/shard_manager/mod.rs
+++ src/shard_manager/mod.rs
@@ -1,13 +1,34 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use serenity::{
 	client::bridge::gateway::ShardManager,
 	prelude::Mutex,
 	prelude::TypeMapKey,
 };
+use tokio::time::sleep;
 
 pub struct ShardManagerContainer;
 
 impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
+}
+
+pub fn shard_iterator_task(manager: Arc<Mutex<ShardManager>>) {
+	tokio::spawn(async move {
+		loop {
+			sleep(Duration::from_secs(60)).await;
+	
+			let lock = manager.lock().await;
+			let shard_runners = lock.runners.lock().await;
+	
+			for (id, runner) in shard_runners.iter() {
+				println!(
+					"Shard ID {} is {} with a latency of {:?}",
+					id,
+					runner.stage,
+					runner.latency,
+				);
+			}
+		}
+	});
 }
