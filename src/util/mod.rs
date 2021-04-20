dc969b7908b8c67c760afd97878afc0293ef095a
Clarity <marcosmejias@mailfence.com>
2021-04-21 00:31:44 +0800

    irrelevant

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,17 +1,16 @@
 use std::num::ParseIntError;
 use serenity::{model::prelude::User, utils::parse_username};
+use chrono::{Datelike, Timelike, Utc};
 
 pub fn utc_date_now_string() -> String {
-	use chrono::{Datelike, Timelike, Utc};
 	let now = Utc::now();
-	format!("{}-{:02}-{:02} {:02}:{:02}:{:02} {:?}",
+	format!("{}-{:02}-{:02} {:02}:{:02}:{:02}",
 		now.year(),
 		now.month(),
 		now.day(),
 		now.hour(),
 		now.minute(),
 		now.second(),
-		now.timezone()
 	)
 }
 
