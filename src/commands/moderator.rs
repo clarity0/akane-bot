867acb262f7ff7352f0169fbbbb8079aeca25d28
Clarity <marcosmejias@mailfence.com>
2021-04-23 06:28:24 +0800

    reorganizing

--- src/commands/moderator.rs
+++ src/commands/moderator.rs
@@ -1,5 +1,4 @@
 use std::str::FromStr;
-
 use serenity::{
 	client::Context,
 	framework::standard::{
@@ -22,6 +21,35 @@ use crate::{database::*, util::*};
 struct Moderator;
 
 #[command]
+#[aliases(dox,)]
+async fn uinfo(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+	if let Ok(user_id) = UserId::from_str(args.message()) {
+		let guild = msg.guild(&ctx.cache).await.ok_or("Error retrieving guild")?;
+		if let Ok(member) = guild.member(&ctx, user_id).await {
+			msg.channel_id.send_message(&ctx, |m| {
+				m.embed(|e| e
+					.title(format!("{}",member.distinct()))
+					.description("User Info")
+					.thumbnail(member.user.avatar_url().unwrap())
+					.field("UserID", user_id.to_string(), false)
+					.field("Nick", member.distinct(), false)
+					.footer(|f| f
+						.text(&msg.author.name)
+						.icon_url(&msg.author.avatar_url().unwrap())
+					)
+					.color(Color::FABLED_PINK)
+				)	
+			}).await?;
+		} else {
+			log_command(Log::Error("user not found"), &ctx, &msg).await?;
+		}
+	} else {
+		log_command(Log::Error("bad user format"), &ctx, &msg).await?;
+	}
+	Ok(())
+}
+
+#[command]
 #[aliases(exile,)]
 async fn ban(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 	if let Ok(user_id) = UserId::from_str(args.message()) {
@@ -34,30 +62,6 @@ async fn ban(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 					log_command(Log::Error(format!("could not update database {}", err).as_str()), &ctx, &msg).await?;
 				} else {
 					log_command(Log::Success(format!("banned user {}", user.tag()).as_str()), &ctx, &msg).await?;
-				}
-			}
-		} else {
-			log_command(Log::Error("user not found"), &ctx, &msg).await?;
-		}
-	} else {
-		log_command(Log::Error("bad user format"), &ctx, &msg).await?;
-	}
-	Ok(())
-}
-
-#[command]
-#[aliases(repatriate,)]
-async fn unban(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-	if let Ok(user_id) = UserId::from_str(args.message()) {
-		if let Ok(user) = user_id.to_user(&ctx).await {
-			let guild = msg.guild(&ctx.cache).await.ok_or("Error retrieving guild")?;
-			if let Err(err) = guild.unban(&ctx, &user).await {
-				log_command(Log::Error(format!("could not unban user {} {}", user.tag(), err).as_str()), &ctx, &msg).await?;
-			} else {
-				if let Err(err) = log_unban(&user, guild) {
-					log_command(Log::Error(format!("could not update database {}", err).as_str()), &ctx, &msg).await?;
-				} else {
-					log_command(Log::Success(format!("unbanned user {}", user.tag()).as_str()), &ctx, &msg).await?;
 				}
 			}
 		} else {
@@ -99,6 +103,30 @@ async fn mute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 }
 
 #[command]
+#[aliases(repatriate,)]
+async fn unban(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+	if let Ok(user_id) = UserId::from_str(args.message()) {
+		if let Ok(user) = user_id.to_user(&ctx).await {
+			let guild = msg.guild(&ctx.cache).await.ok_or("Error retrieving guild")?;
+			if let Err(err) = guild.unban(&ctx, &user).await {
+				log_command(Log::Error(format!("could not unban user {} {}", user.tag(), err).as_str()), &ctx, &msg).await?;
+			} else {
+				if let Err(err) = log_unban(&user, guild) {
+					log_command(Log::Error(format!("could not update database {}", err).as_str()), &ctx, &msg).await?;
+				} else {
+					log_command(Log::Success(format!("unbanned user {}", user.tag()).as_str()), &ctx, &msg).await?;
+				}
+			}
+		} else {
+			log_command(Log::Error("user not found"), &ctx, &msg).await?;
+		}
+	} else {
+		log_command(Log::Error("bad user format"), &ctx, &msg).await?;
+	}
+	Ok(())
+}
+
+#[command]
 #[aliases(unsilence,)]
 async fn unmute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 	if let Ok(user_id) = UserId::from_str(args.message()) {
@@ -123,35 +151,6 @@ async fn unmute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 			} else {
 				log_command(Log::Error("muted role does not exist"), &ctx, &msg).await?;
 			}
-		} else {
-			log_command(Log::Error("user not found"), &ctx, &msg).await?;
-		}
-	} else {
-		log_command(Log::Error("bad user format"), &ctx, &msg).await?;
-	}
-	Ok(())
-}
-
-#[command]
-#[aliases(dox,)]
-async fn uinfo(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-	if let Ok(user_id) = UserId::from_str(args.message()) {
-		let guild = msg.guild(&ctx.cache).await.ok_or("Error retrieving guild")?;
-		if let Ok(member) = guild.member(&ctx, user_id).await {
-			msg.channel_id.send_message(&ctx, |m| {
-				m.embed(|e| e
-					.title(format!("{}",member.distinct()))
-					.description("User Info")
-					.thumbnail(member.user.avatar_url().unwrap())
-					.field("UserID", user_id.to_string(), false)
-					.field("Nick", member.distinct(), false)
-					.footer(|f| f
-						.text(&msg.author.name)
-						.icon_url(&msg.author.avatar_url().unwrap())
-					)
-					.color(Color::FABLED_PINK)
-				)	
-			}).await?;
 		} else {
 			log_command(Log::Error("user not found"), &ctx, &msg).await?;
 		}
