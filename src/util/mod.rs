1b051c53e37a0ec00ce41feb547e114e7336422b
Clarity <marcosmejias@mailfence.com>
2021-04-25 03:55:23 +0800

    tweaked superficial things commands

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,4 +1,4 @@
-use serenity::{client::Context, framework::standard::CommandResult, model::{channel::Message}};
+use serenity::{client::Context, framework::standard::CommandResult, model::{channel::Message}, utils::Color};
 pub enum Log<'a> {
 	Success(&'a str),
 	Error(&'a str),
@@ -7,7 +7,10 @@ pub enum Log<'a> {
 pub async fn log_command<'a>(message: Log<'a>, ctx: &Context, cmd_msg: &Message) -> CommandResult {
 	match message {
 		Log::Success(message) => {
-			cmd_msg.channel_id.say(&ctx, format!("Success: {}",message)).await?;
+			cmd_msg.channel_id.send_message(&ctx, |m| {
+				m.content(format!("Success: {}",message))
+				.reference_message(cmd_msg)
+			}).await?;
 		},
 		Log::Error(message) => {
 			eprintln!("ERROR {}", message);
@@ -16,13 +19,17 @@ pub async fn log_command<'a>(message: Log<'a>, ctx: &Context, cmd_msg: &Message)
 			}
 			if let Some(guild) = cmd_msg.guild(&ctx).await {
 				if let Some(channel_id) = guild.channel_id_from_name(&ctx, "akane-logging").await {
+					if let Some(avatar_url) = cmd_msg.author.avatar_url() {
 					let cmd_msg = cmd_msg.content_safe(&ctx).await;
 					channel_id.send_message(&ctx, |m| m
 						.embed(|e| e
 							.title("Command Error")
 							.description(message)
 							.field("Command", cmd_msg, false)
+							.thumbnail(avatar_url)
+							.color(Color::RED)
 					)).await?;
+					}
 				}
 			}
 		}
