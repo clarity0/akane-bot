3203c39f4db43be9bfdf08d3139ed83405b39110
Clarity <marcosmejias@mailfence.com>
2021-04-29 18:25:28 +0800

    removed duplicated migrations

--- src/migrations/2021-04-24-211524_gulag/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE gulaglist (
-	user_id VARCHAR(18) PRIMARY KEY,
-	server_id VARCHAR(18) NOT NULL,
-	user_handle VARCHAR(32) NOT NULL,
-	date TIMESTAMP NOT NULL
-);
