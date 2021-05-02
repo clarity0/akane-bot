2b87f99fea4d5fbc356038e08de4f5b6e63cda58
Clarity <marcosmejias@mailfence.com>
2021-05-02 18:16:42 +0800

    added join command

--- src/commands/voice.rs
+++ src/commands/voice.rs
@@ -2,11 +2,10 @@ use serenity::{client::Context, framework::standard::{CommandResult, macros::{co
 
 #[group]
 #[only_in(guilds)]
-#[commands(deafen,)]
+#[commands(deafen, join)]
 struct Voice;
 
 #[command]
-#[only_in(guilds)]
 async fn deafen(ctx: &Context, msg: &Message) -> CommandResult {
     let guild = msg.guild(&ctx.cache).await.ok_or("could not retrieve guild")?;
 
@@ -21,11 +20,37 @@ async fn deafen(ctx: &Context, msg: &Message) -> CommandResult {
     	} else if let Err(e) = handler.deafen(true).await {
             msg.channel_id.say(&ctx.http, format!("Failed: {:?}", e)).await?;
     	}
-		
+
         msg.channel_id.say(&ctx.http, "Deafened").await?;
     } else {
 		msg.reply(ctx, "Not in a voice channel").await?;
 	}
+
+    Ok(())
+}
+
+#[command]
+async fn join(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).await.unwrap();
+    let guild_id = guild.id;
+
+    let channel_id = guild
+        .voice_states.get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            msg.reply(ctx, "Not in a voice channel").await?;
+
+            return Ok(());
+        }
+    };
+
+    let manager = songbird::get(ctx).await
+        .expect("Songbird Voice client placed in at initialisation.").clone();
+
+    let _handler = manager.join(guild_id, connect_to).await;
 
     Ok(())
 }
